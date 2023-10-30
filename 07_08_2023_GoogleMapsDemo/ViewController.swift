8804db9389aa2e316ed391c3f9b760e068378572
aishwarya <ashp17star@gmail.com>
2023-10-30 11:33:10 +0530

    Circle, Polygon, Polyline Drawing on Map

--- 07_08_2023_GoogleMapsDemo/ViewController.swift
+++ 07_08_2023_GoogleMapsDemo/ViewController.swift
@@ -21,18 +21,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         initializationOfGoogleMapView()
-        showMarker(position: punePosition)
         setttingUpCameraPosition(position: punePosition)
+        showMarker(position: punePosition)
+        drawPolyline()
+        drawCircle(position: punePosition)
+        drawPolygon()
+        
 //        GMSCameraUpdate.setTarget(
 //            punePosition,
 //            zoom: 30.0)
     }
+    
     func setttingUpCameraPosition(position : CLLocationCoordinate2D){
         
         cameraPosition = GMSCameraPosition(
             latitude:position.latitude,
             longitude: position.longitude,
-            zoom: 30.0)
+            zoom: 5.0)
         googleMapView.camera = cameraPosition!
     }
     
@@ -53,9 +58,54 @@ class ViewController: UIViewController {
         marker.rotation = 30.0
         marker.zIndex = 10
         marker.isDraggable = true
-        marker.title = "Bitcode"
+        marker.title = "Pune"
         marker.map = googleMapView
-        marker.snippet = "Marker Bitcode"
+        marker.snippet = "Marker Pune"
         marker.position = position
     }
+    
+    func drawPolygon(){
+        let gmsMutablePolygonPath = GMSMutablePath()
+        gmsMutablePolygonPath.add(CLLocationCoordinate2D(latitude: 26.90, longitude: 75.73))
+        gmsMutablePolygonPath.add(CLLocationCoordinate2D(latitude: 24.87, longitude: 74.62))
+        gmsMutablePolygonPath.add(CLLocationCoordinate2D(latitude: 12.12, longitude: 76.68))
+        gmsMutablePolygonPath.add(CLLocationCoordinate2D(latitude: 16.99, longitude: 73.30))
+        
+        let polygonPath = GMSPolygon(path: gmsMutablePolygonPath)
+        polygonPath.strokeWidth = 5.0
+        polygonPath.strokeColor = .brown
+        polygonPath.fillColor = UIColor(red: 50.0, green: 0.0, blue: 50.0, alpha: 3.0)
+        polygonPath.map = googleMapView
+    }
+    
+    func drawPolyline(){
+        let gmsMutablePolylinePath = GMSMutablePath()
+        gmsMutablePolylinePath.add(CLLocationCoordinate2D(latitude: 19.07, longitude: 72.87))
+        gmsMutablePolylinePath.add(CLLocationCoordinate2D(latitude: 19.87, longitude: 75.34))
+        gmsMutablePolylinePath.add(CLLocationCoordinate2D(latitude: 19.14, longitude: 77.31))
+        gmsMutablePolylinePath.add(CLLocationCoordinate2D(latitude: 18.52, longitude: 73.85))
+        gmsMutablePolylinePath.add(CLLocationCoordinate2D(latitude: 19.07, longitude: 72.87))
+        
+        let gmsPolylinePath = GMSPolyline(path: gmsMutablePolylinePath)
+        gmsPolylinePath.strokeColor = .black
+        gmsPolylinePath.strokeWidth = 20.0
+        gmsPolylinePath.map = googleMapView
+        gmsPolylinePath.title = "Polyline"
+    }
+    
+    func drawCircle(position : CLLocationCoordinate2D){
+    
+        let circle = GMSCircle(
+            position: position,
+            radius: 1000.0)
+        circle.title = "bitcode"
+        circle.fillColor = .lightGray
+        circle.strokeColor = .magenta
+        circle.strokeWidth = 4.0
+        circle.map = googleMapView
+    }
+}
+
+extension ViewController : GMSMapViewDelegate{
+    
 }
